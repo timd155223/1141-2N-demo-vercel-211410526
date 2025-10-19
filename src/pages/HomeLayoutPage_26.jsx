1624f2bc15b1b1b87414b9855146e65323d56858
timd155223 <timd155223@gmail.com>
2025-10-19 14:08:27 +0800

    third commit

--- src/pages/HomeLayoutPage_26.jsx
+++ src/pages/HomeLayoutPage_26.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Navbar_26 from '../components/navbar_26.jsx';
+import Navbar_26 from '../components/Navbar_26.jsx';
 
 const HomeLayoutPage_26 = () => {
   return (
