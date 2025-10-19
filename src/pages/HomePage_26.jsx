5478a0ce41ed8e5083e52f03acd0f7b37d2ff5c4
timd155223 <timd155223@gmail.com>
2025-10-19 14:48:46 +0800

    add tailwindcss support

--- src/pages/HomePage_26.jsx
+++ src/pages/HomePage_26.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const HomePage_26 = () => {
   return (
-    <div>
-      <h1>HomePage_26</h1>
-      <h2>timd, 211410526</h2>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-2xl text-indigo-600'>HomePage_26</h1>
+      <h2 className='text-xl m-16'>timd, 211410526</h2>
     </div>
   );
 };
